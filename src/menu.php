f38f5f472292a32ff05fde1905f51752c3b8be3c
aespakari7 <147119919+aespakari7@users.noreply.github.com>
2024-01-12 13:58:13 +0900

    menu

--- src/menu.php
+++ src/menu.php
@@ -7,11 +7,9 @@
 </head>
 <body>
 <h1>選手管理メニュー</h1>
-    <form action="plyer-output.php" method="post">
-        <button type="submit" name="select">一覧</button>
-        <button type="submit" name="insert">登録</button>
-        <button type="submit" name="update">更新</button>
-        <button type="submit" name="delete">削除</button>
-    </form>
+        <a href="player-select.php"><button type="submit" name="select">一覧</button></a>
+        <a href="player-insert.php"><button type="submit" name="insert">登録</button></a>
+        <a href="player-update.php"><button type="submit" name="update">更新</button></a>
+        <a href="pleyer-delete.php"><button type="submit" name="delete">削除</button></a>
 </body>
 </html>
