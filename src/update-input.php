0c6ed3ee53db961c69967e2f2e03bd8ab13e56f7
aespakari7 <147119919+aespakari7@users.noreply.github.com>
2024-01-17 14:01:58 +0900

    a

--- src/update-input.php
+++ src/update-input.php
@@ -1,7 +1,10 @@
 <?php
 require 'db-connect.php';
 $pdo=new PDO($connect,USER,PASS);
-    $sql=$pdo->query('select * from club');
+    $sql=$pdo->prepare('select * from player where player_name = ?');
+    $sql->execute([$_GET['player_name']]);
+    $row2 = $sql->fetch();
+    $sql=$pdo->prepare('select * from club');
 ?>
 
 <!DOCTYPE html>
@@ -18,15 +21,19 @@ $pdo=new PDO($connect,USER,PASS);
 <a href="delete.php">選手情報を削除する</a>
 <hr>
 <form action="update-output.php" method="post">
-    選手名：<input type="text" name="player_name">
+    選手名：<input type="text" name="player_name" value="<?= $row2['player_name']?>">
     <br>
-    国籍：<input type="text" name="country_name">
+    国籍：<input type="text" name="country_name" value="<?= $row2['country_name']?>>
     <br>
     所属クラブ：
     <select name="club_name">
 <?php
     foreach($sql as $row){
-        echo '<option value="',$row['club_id'],'">',$row['club_name'];
+        $select ="";
+        if( $row2['club_id'] == $row['club_id']){
+            $select ="selected";
+        }
+        echo '<option value="',$row['club_id'],'" ',$selected,'>',$row['club_name'];
     }
 ?>
 </select>
